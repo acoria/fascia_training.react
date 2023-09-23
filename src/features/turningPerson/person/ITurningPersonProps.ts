13d84dd5572cd51cfa28a441fe118bd60a5782a0
acoria <tewes.verena@gmail.com>
2023-09-23 22:14:09 +0200

    Replace person.png with svg file

--- src/features/turningPerson/person/ITurningPersonProps.ts
+++ src/features/turningPerson/person/ITurningPersonProps.ts
@@ -1,3 +1,4 @@
 export interface ITurningPersonProps {
-    className?: string
+  className?: string;
+  personColor?: string;
 }
--- src/features/turningPerson/person/TurningPerson.tsx
+++ src/features/turningPerson/person/TurningPerson.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import styles from "./TurningPerson.module.css";
-import person from "../assets/person.png";
-import { Buttock } from "../buttock/Buttock";
-import { WinkingEye } from "../eye/winkingEye/WinkingEye";
 import { CSSTransition } from "react-transition-group";
 import { Icon } from "../../../components/icon/Icon";
 import { MaterialIcons } from "../../../components/icon/MaterialIcons";
+import { ReactComponent as PersonImage } from "../assets/person.svg";
+import { Buttock } from "../buttock/Buttock";
+import { WinkingEye } from "../eye/winkingEye/WinkingEye";
 import { ITurningPersonProps } from "./ITurningPersonProps";
+import styles from "./TurningPerson.module.css";
 
 export const TurningPerson: React.FC<ITurningPersonProps> = (props) => {
   const [showFront, setShowFront] = useState(true);
@@ -20,20 +20,29 @@ export const TurningPerson: React.FC<ITurningPersonProps> = (props) => {
         enter: styles.personShrink,
         enterDone: styles.personShrunk,
         exitActive: styles.personShrunk,
-        exit: styles.personGrow
+        exit: styles.personGrow,
       }}
     >
       <div className={`${styles.turningPerson} ${props.className}`}>
-        <img
-          className={`${styles.image} ${showFront ? "" : styles.flipSideways}`}
-          src={person}
-          alt="Person"
-          onClick={() => setShowFullSize((previous) => !previous)}
-        />
+        <div>
+          <PersonImage
+            style={{ color: props.personColor ?? "black" }}
+            className={`${styles.image} ${
+              showFront ? "" : styles.flipSideways
+            }`}
+            onClick={() => setShowFullSize((previous) => !previous)}
+          />
+        </div>
         <Buttock show={!showFront} />
         <WinkingEye show={showFront} positionInPercentFromLeft={50} />
         <WinkingEye show={showFront} positionInPercentFromLeft={60} />
-        {showFullSize && <Icon icon={MaterialIcons.refresh} className={styles.turnButton} onClick={() => setShowFront((previous) => !previous)} />}
+        {showFullSize && (
+          <Icon
+            icon={MaterialIcons.refresh}
+            className={styles.turnButton}
+            onClick={() => setShowFront((previous) => !previous)}
+          />
+        )}
       </div>
     </CSSTransition>
   );
